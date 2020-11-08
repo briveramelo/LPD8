74067618584d00886f93bd53c501f3be300839a9
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2020-11-07 17:35:03 -0700

    initial setup switching main to just use default setup, commenting out unwanted variations. added pygame library for multiple concurrent sounds. Just added a single sound for now, will set up an array of 8 sounds for each pad in each program. for some reason, only program3 is set up to map pad numbers properly with the Pads class. Will refactor this to allow use of each program with correct pad numbers and no errors

--- consummer.py
+++ consummer.py
@@ -1,9 +1,15 @@
+import pygame
+
 from lpd8.pgm_chg import Pgm_Chg
+
 
 class Consummer:
 
     def __init__(self):
         self._running = True
+        pygame.mixer.init(channels=8)
+        self.sndA = pygame.mixer.Sound("./sounds/special_explosion.wav")
+        self.soundChannelA = pygame.mixer.Channel(1)
 
     def is_running(self):
         return self._running
@@ -16,8 +22,13 @@ class Consummer:
 
     def note_on_value(self, data):
         print('NOTE ON : ' + str(data))
+        maxVelocity = 127
+        normalizedVolume = data[2] / maxVelocity
+        self.soundChannelA.set_volume(normalizedVolume)
+        self.soundChannelA.play(self.sndA)
 
     def note_off_value(self, data):
+        self.soundChannelA.stop()
         print('NOTE OFF : ' + str(data))
 
     def pgm_change(self, data):
--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from lpd8.pgm_chg import Pgm_Chg
 from consummer import Consummer
 from time import sleep
 
-# This object is created to test different callbacks from LPD8 class
-consummer = Consummer()
-
 # Create an LPD8 object and try to start it (open MIDI in and out communication)
 lpd8 = LPD8()
 lpd8.start()
@@ -17,17 +14,17 @@ lpd8.start()
 # value, knob will not react when we return to original program until last stored value for this program
 # will be reached. It will then follow changes normally
 # Note that we can define sticky mode for a single knob, an array of knobs or all knobs
-lpd8.set_not_sticky_knob(Programs.PGM_4, [Knobs.KNOB_1, Knobs.KNOB_2, Knobs.KNOB_3, Knobs.KNOB_4])
+# lpd8.set_not_sticky_knob(Programs.PGM_3, [Knobs.KNOB_1, Knobs.KNOB_2, Knobs.KNOB_3, Knobs.KNOB_4])
 
 # In all following settings, we will define limits / actions for PROGRAM 4
 # Define control knob 1 limits from -1 to 1 and set increments to float values
 # Define control knob 2 limits from 0 to 100 with 10 steps (limit values to 0, 10, 20, ..., 90, 100)
 # Knobs that have no definition range from 0..127 with integer increments of 1 (default MIDI behaviour)
-lpd8.set_knob_limits(Programs.PGM_4, Knobs.KNOB_1, -1, 1, is_int=False)
-lpd8.set_knob_limits(Programs.PGM_4, Knobs.KNOB_2, 0, 100, steps=10)
+# lpd8.set_knob_limits(Programs.PGM_3, Knobs.KNOB_1, -1, 1, is_int=False)
+# lpd8.set_knob_limits(Programs.PGM_3, Knobs.KNOB_2, 0, 100, steps=10)
 
 # Set An initial value for knob 3 to 63
-lpd8.set_knob_value(Programs.PGM_4, Knobs.KNOB_3, 63)
+# lpd8.set_knob_value(Programs.PGM_3, Knobs.KNOB_3, 63)
 
 # Set different modes for pads
 # Note that we can define modes for a single pad, an array of pads or all pads
@@ -35,20 +32,24 @@ lpd8.set_knob_value(Programs.PGM_4, Knobs.KNOB_3, 63)
 # Pad 3 won't blink but will be in switch mode too
 # Pad 4 will be in push mode, sending a 1 at every NOTE ON event and a 0 at every NOTE OFF event
 # Pads that have no definition are set in classical pad mode with NOTE ON / NOTE OFF events and velocity values
-lpd8.set_pad_mode(Programs.PGM_4, [Pads.PAD_1, Pads.PAD_2], Pad.SWITCH_MODE + Pad.BLINK_MODE)
-lpd8.set_pad_mode(Programs.PGM_4, Pads.PAD_3, Pad.SWITCH_MODE)
-lpd8.set_pad_mode(Programs.PGM_4, Pads.PAD_4, Pad.PUSH_MODE)
+# lpd8.set_pad_mode(Programs.PGM_3, [Pads.PAD_1, Pads.PAD_2], Pad.PAD_MODE)
+# lpd8.set_pad_mode(Programs.PGM_3, Pads.PAD_3, Pad.SWITCH_MODE)
+# lpd8.set_pad_mode(Programs.PGM_3, Pads.PAD_4, Pad.PUSH_MODE)
 
 # For a pad of type SWITCH, sets the initial state of pad
 # Note that we can define modes for a single pad, an array of pads or all pads
-lpd8.set_pad_switch_state(Programs.PGM_4, [Pads.PAD_1, Pads.PAD_3], Pad.ON)
+# lpd8.set_pad_switch_state(Programs.PGM_3, [Pads.PAD_1, Pads.PAD_3], Pad.ON)
 
 # Subscribe to different events and map them to a method in test object
 # Note that we can subscribe events for a single object, an array of objects or all objects of a group
-lpd8.subscribe(consummer, consummer.ctrl_value, Programs.PGM_4, LPD8.CTRL, Knobs.ALL_KNOBS)
-lpd8.subscribe(consummer, consummer.note_on_value, Programs.PGM_4, LPD8.NOTE_ON, Pads.ALL_PADS)
-lpd8.subscribe(consummer, consummer.note_off_value, Programs.PGM_4, LPD8.NOTE_OFF, Pads.ALL_PADS)
-lpd8.subscribe(consummer, consummer.pgm_change, Programs.PGM_4, LPD8.PGM_CHG, Pgm_Chg.PGM_CHG_4)
+
+# This object is created to test different callbacks from LPD8 class
+consummer = Consummer()
+
+lpd8.subscribe(consummer, consummer.ctrl_value, Programs.PGM_3, LPD8.CTRL, Knobs.ALL_KNOBS)
+lpd8.subscribe(consummer, consummer.note_on_value, Programs.PGM_3, LPD8.NOTE_ON, Pads.ALL_PADS)
+lpd8.subscribe(consummer, consummer.note_off_value, Programs.PGM_3, LPD8.NOTE_OFF, Pads.ALL_PADS)
+# lpd8.subscribe(consummer, consummer.pgm_change, Programs.PGM_3, LPD8.PGM_CHG, Pgm_Chg.PGM_CHG_3)
 
 # We loop as long as test class allows it
 while consummer.is_running():
